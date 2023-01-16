8ffe0e1ac524a2cf97d5e46e4dbe78209082773d
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-16 18:29:46 -0500

    VO [Username]=Nombre de usuario -> Status: Terminado

--- Domain/src/main/java/com/sofka/shop/values/Username.java
+++ Domain/src/main/java/com/sofka/shop/values/Username.java
@@ -1,4 +1,22 @@
 package com.sofka.shop.values;
 
-public class Username {
+import co.com.sofka.domain.generic.ValueObject;
+
+public class Username implements ValueObject<String> {
+
+    private final String alias;
+
+    public Username(String alias) {
+        this.alias = alias;
+    }
+
+    public static Username of(String alias) {
+        return new Username(alias);
+    }
+
+    @Override
+    public String value() {
+        return alias;
+    }
+
 }
