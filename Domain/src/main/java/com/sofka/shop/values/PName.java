0b8c4074c72b51a59750d72c81200a692a5db47b
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-16 17:38:53 -0500

    VO [PName]=Nombre Producto -> Status: Terminado

--- Domain/src/main/java/com/sofka/shop/values/PName.java
+++ Domain/src/main/java/com/sofka/shop/values/PName.java
@@ -1,4 +1,22 @@
 package com.sofka.shop.values;
 
-public class PName {
+import co.com.sofka.domain.generic.ValueObject;
+
+public class PName implements ValueObject<String> {
+
+    private final String name;
+
+    public PName(String name) {
+        this.name = name;
+    }
+
+    public static PName of(String name) {
+        return new PName(name);
+    }
+
+    @Override
+    public String value() {
+        return name;
+    }
+
 }
