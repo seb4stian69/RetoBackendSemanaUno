0051cdbfcb088f2791bb71e7b1aae30a16dbc702
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-16 18:56:49 -0500

    Entity [Product]=Producto -> Status: Terminado

--- Domain/src/main/java/com/sofka/shop/entities/Product.java
+++ Domain/src/main/java/com/sofka/shop/entities/Product.java
@@ -1,4 +1,23 @@
 package com.sofka.shop.entities;
 
-public class Product {
+import co.com.sofka.domain.generic.Entity;
+import com.sofka.shop.values.*;
+
+public class Product extends Entity<IDProduct> {
+
+    private final PName name;
+    private InInventory inInventory;
+    private Enable isEnabled;
+    private final Max max;
+    private final Min min;
+
+    public Product(IDProduct id, PName name, InInventory inInventory, Enable isEnabled, Max max, Min min) {
+        super(id);
+        this.name = name;
+        this.inInventory = inInventory;
+        this.isEnabled = isEnabled;
+        this.max = max;
+        this.min = min;
+    }
+
 }
