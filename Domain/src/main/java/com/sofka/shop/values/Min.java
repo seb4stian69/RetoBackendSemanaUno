f7d5cd33bfecbcf2220ee429eff42b65ea1ae522
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-16 18:26:59 -0500

    VO [Min]=Minimacantidad de productos -> Status: Terminado

--- Domain/src/main/java/com/sofka/shop/values/Min.java
+++ Domain/src/main/java/com/sofka/shop/values/Min.java
@@ -1,4 +1,22 @@
 package com.sofka.shop.values;
 
-public class Min {
+import co.com.sofka.domain.generic.ValueObject;
+
+public class Min implements ValueObject<Integer> {
+
+    private final Integer quantity;
+
+    public Min(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public static Min of(Integer quantity) {
+        return new Min(quantity);
+    }
+
+    @Override
+    public Integer value() {
+        return quantity;
+    }
+
 }
