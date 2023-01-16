a21f60baafa64ce0660ab351039c243d34fb9541
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-16 17:55:27 -0500

    VO [Enable]=Producto disponible -> Status: Terminado

--- Domain/src/main/java/com/sofka/shop/values/Enable.java
+++ Domain/src/main/java/com/sofka/shop/values/Enable.java
@@ -1,4 +1,22 @@
 package com.sofka.shop.values;
 
-public class Enable {
+import co.com.sofka.domain.generic.ValueObject;
+
+public class Enable implements ValueObject<Boolean> {
+
+    private final Boolean isEnable;
+
+    public Enable(Boolean isEnable) {
+        this.isEnable = isEnable;
+    }
+
+    public static Enable of(Boolean isEnable) {
+        return new Enable(isEnable);
+    }
+
+    @Override
+    public Boolean value() {
+        return isEnable;
+    }
+
 }
