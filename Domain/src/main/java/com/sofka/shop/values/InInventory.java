f1238e4579495b8ab552a878af89df86464623d1
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-16 17:57:46 -0500

    VO [InInventory]=Cantidad de productos disponible -> Status: Terminado

--- Domain/src/main/java/com/sofka/shop/values/InInventory.java
+++ Domain/src/main/java/com/sofka/shop/values/InInventory.java
@@ -1,4 +1,22 @@
 package com.sofka.shop.values;
 
-public class InInventory {
+import co.com.sofka.domain.generic.ValueObject;
+
+public class InInventory implements ValueObject<Integer> {
+
+    private final Integer quantity;
+
+    public InInventory(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public static InInventory of(Integer quantity) {
+        return new InInventory(quantity);
+    }
+
+    @Override
+    public Integer value() {
+        return quantity;
+    }
+
 }
