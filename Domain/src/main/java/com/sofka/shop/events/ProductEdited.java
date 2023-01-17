8b2431c2cb882b6a439b886702833b4264312e64
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-17 13:39:48 -0500

    Events [ProductEdited]= Producto Editado -> Status: Modificado

--- Domain/src/main/java/com/sofka/shop/events/ProductEdited.java
+++ Domain/src/main/java/com/sofka/shop/events/ProductEdited.java
@@ -1,29 +1,30 @@
 package com.sofka.shop.events;
 
 import co.com.sofka.domain.generic.DomainEvent;
+import com.sofka.shop.values.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter/* */@Setter
 public class ProductEdited extends DomainEvent {
 
-    private String name;
-    private Integer inINventory;
-    private Boolean enabled;
-    private Integer max;
-    private Integer min;
+    private ProductID productID;
+    private PName name;
+    private InInventory inINventory;
+    private Enable enabled;
+    private Max max;
+    private Min min;
+    private Price price;
 
-    public ProductEdited(){
+    public ProductEdited(ProductID productID, PName name, InInventory inINventory, Enable enabled, Max max, Min min, Price price) {
         super("shop.productEdited");
-    }
-
-    public ProductEdited(String name, Integer inINventory, Boolean enabled, Integer max, Integer min){
-        super("shop.productEdited");
+        this.productID = productID;
         this.name = name;
         this.inINventory = inINventory;
         this.enabled = enabled;
         this.max = max;
         this.min = min;
+        this.price = price;
     }
 
 }
