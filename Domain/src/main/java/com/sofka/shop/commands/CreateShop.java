f825ec9e8bbf6a84d95a9bda083a03e5d189cf4e
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-17 00:03:39 -0500

    Command (CreateShop)= Crear tienda -> status: Modified

--- Domain/src/main/java/com/sofka/shop/commands/CreateShop.java
+++ Domain/src/main/java/com/sofka/shop/commands/CreateShop.java
@@ -1,18 +1,18 @@
 package com.sofka.shop.commands;
 
 import co.com.sofka.domain.generic.Command;
+import com.sofka.shop.entities.Product;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.Map;
+import java.util.Set;
 
 @Getter/* */@Setter
 @AllArgsConstructor/* */@NoArgsConstructor
 
 public class CreateShop extends Command {
 
-    private Map<String, Integer> products;
+    private Set<Product> products;
 
 }
