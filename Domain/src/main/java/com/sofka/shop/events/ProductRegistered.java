21f25016599723bf28cba5f34b0f428772c264c1
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-16 21:14:03 -0500

    Events [ProductRegistered] = Producto registrado -> status: Actualizado

--- Domain/src/main/java/com/sofka/shop/events/ProductRegistered.java
+++ Domain/src/main/java/com/sofka/shop/events/ProductRegistered.java
@@ -1,23 +1,24 @@
 package com.sofka.shop.events;
 
 import co.com.sofka.domain.generic.DomainEvent;
+import com.sofka.shop.values.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter/* */@Setter
 public class ProductRegistered extends DomainEvent {
 
-    private String name;
-    private Integer inINventory;
-    private Boolean enabled;
-    private Integer max;
-    private Integer min;
+    private PName name;
+    private InInventory inINventory;
+    private Enable enabled;
+    private Max max;
+    private Min min;
 
     public ProductRegistered(){
         super("shop.productregistered");
     }
 
-    public ProductRegistered(String name, Integer inINventory, Boolean enabled, Integer max, Integer min){
+    public ProductRegistered(PName name, InInventory inINventory, Enable enabled, Max max, Min min){
         super("shop.productregistered");
         this.name = name;
         this.inINventory = inINventory;
