4118e130c619f6ede10289dab496fa99336bf63b
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-17 14:32:51 -0500

    Events [ShopCreated]= Tienda creada -> Status: Modificado

--- Domain/src/main/java/com/sofka/shop/events/ShopCreated.java
+++ Domain/src/main/java/com/sofka/shop/events/ShopCreated.java
@@ -4,18 +4,18 @@ import co.com.sofka.domain.generic.DomainEvent;
 import com.sofka.shop.entities.Product;
 import lombok.Getter;
 import lombok.Setter;
-import java.util.Set;
+import java.util.List;
 
 @Getter/* */@Setter
 public class ShopCreated extends DomainEvent {
 
-    private Set<Product> products;
+    private List<Product> products;
 
     public ShopCreated() {
         super("shop.createdShop");
     }
 
-    public ShopCreated(Set<Product> products) {
+    public ShopCreated(List<Product> products) {
         super("shop.createdShop");
         this.products = products;
     }
