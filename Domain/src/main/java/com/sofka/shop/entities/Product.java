b2f71ce2064b5b0e89314a3fd347a0a584e4e848
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-17 15:54:06 -0500

    Entities [Product]= Producto -> Status: Modificado

--- Domain/src/main/java/com/sofka/shop/entities/Product.java
+++ Domain/src/main/java/com/sofka/shop/entities/Product.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 @Getter/* */@Setter
 public class Product extends Entity<IDProduct> {
 
+    private final IDProduct productID;
     private final PName name;
     private InInventory inInventory;
     private Enable isEnabled;
@@ -17,6 +18,7 @@ public class Product extends Entity<IDProduct> {
 
     public Product(IDProduct id, PName name, InInventory inInventory, Enable isEnabled, Max max, Min min, Price price) {
         super(id);
+        this.productID = id;
         this.name = name;
         this.inInventory = inInventory;
         this.isEnabled = isEnabled;
