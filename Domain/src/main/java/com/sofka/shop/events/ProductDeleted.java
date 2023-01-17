e47ca39a0f0acaba9337465bc5caff1c19e4d5f5
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-17 12:58:33 -0500

    Events [ProductDeleted]= Producto Eliminado -> Status: Modificado

--- Domain/src/main/java/com/sofka/shop/events/ProductDeleted.java
+++ Domain/src/main/java/com/sofka/shop/events/ProductDeleted.java
@@ -1,20 +1,20 @@
 package com.sofka.shop.events;
 
 import co.com.sofka.domain.generic.DomainEvent;
+import com.sofka.shop.values.ProductID;
+import com.sofka.shop.values.ShopID;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter/* */@Setter
 public class ProductDeleted extends DomainEvent {
 
-    private String productId;
+    private ShopID shopID;
+    private ProductID productId;
 
-    public ProductDeleted(){
+    public ProductDeleted(ShopID shopID, ProductID productId) {
         super("shop.productDeleted");
-    }
-
-    public ProductDeleted(String productId) {
-        super("shop.productDeleted");
+        this.shopID = shopID;
         this.productId = productId;
     }
 
