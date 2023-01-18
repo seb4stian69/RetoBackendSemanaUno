06832b651fd6e408509cc5fe892100459a309fef
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-18 14:00:22 -0500

    Actualizando el modelo de producto en el gateway del bussinnes

--- Bussines/src/main/java/com/sofka/shop/gateway/ModelProduct.java
+++ Bussines/src/main/java/com/sofka/shop/gateway/ModelProduct.java
@@ -1,4 +1,22 @@
 package com.sofka.shop.gateway;
 
+import com.sofka.shop.values.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter/* */@Setter
+@NoArgsConstructor/* */@AllArgsConstructor
+
 public class ModelProduct {
+
+    private IDProduct productID;
+    private PName name;
+    private InInventory inInventory;
+    private Enable isEnabled;
+    private Max max;
+    private Min min;
+    private Price price;
+
 }
