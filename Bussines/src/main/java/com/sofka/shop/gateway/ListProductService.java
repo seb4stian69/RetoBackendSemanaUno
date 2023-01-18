489b7ab62d9d75961efa2c8b7f97fb0c3fcf77db
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-01-18 13:59:21 -0500

    Actualizando las interfaces [ListProductService and ShopDomainEventRepository]

--- Bussines/src/main/java/com/sofka/shop/gateway/ListProductService.java
+++ Bussines/src/main/java/com/sofka/shop/gateway/ListProductService.java
@@ -1,4 +1,7 @@
 package com.sofka.shop.gateway;
 
+import reactor.core.publisher.Flux;
+
 public interface ListProductService {
+    Flux<ModelProduct> obtenerProductos();
 }
--- Bussines/src/main/java/com/sofka/shop/gateway/ShopDomainEventRepository.java
+++ Bussines/src/main/java/com/sofka/shop/gateway/ShopDomainEventRepository.java
@@ -1,4 +1,10 @@
 package com.sofka.shop.gateway;
 
+import co.com.sofka.domain.generic.DomainEvent;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface ShopDomainEventRepository {
+    Flux<DomainEvent> getEventBy(String id);
+    Mono<Boolean> exist(String id);
 }
